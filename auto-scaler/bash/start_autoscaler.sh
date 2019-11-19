d4f998842d2983aa42fbf01299172f7aef05a2a9
anushiyat <anushiya.16@cse.mrt.ac.lk>
2019-11-19 13:59:07 +0530

    improvements on auto-scaler

--- auto-scaler/bash/start_autoscaler.sh
+++ auto-scaler/bash/start_autoscaler.sh
@@ -5,6 +5,11 @@ CA_CERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 dir="$(pwd)"
 python_file="$(dirname "$dir")/python-helpers/auto-scale-pods.py"
 
+desired_latency=5000
+min_pod=1
+max_pod=20
+filename=$dir/scale.json
+
 runtime="10 minute"
 endtime=$(date -ud "$runtime" +%s)
 
@@ -24,7 +29,7 @@ do
     curl --cacert ${CA_CERT} -H 'Accept: application/json' -H "Authorization: Bearer $(cat ${TOKEN})" ${URL} > scale.json
 
     echo "Updating the scale.json file with required number of pods"
-    python3 $python_file $latency_response
+    python3 $python_file $latency_response $desired_latency $min_pod $max_pod $filename
     
     echo "scaling"
     curl -X PUT -d@scale.json --cacert ${CA_CERT} -H 'Content-Type: application/json' -H "Authorization: Bearer $(cat ${TOKEN})" $URL
--- auto-scaler/python-helpers/auto-scale-pods.py
+++ auto-scaler/python-helpers/auto-scale-pods.py
@@ -8,11 +8,15 @@ from desired_pod_cal import *
 latency_response = sys.argv[1]
 latency_response = json.loads(latency_response)
 
-desired_latency = 5000
-min_pod = 1
-max_pod = 20
+desired_latency = int(sys.argv[2])
+min_pod = int(sys.argv[3])
+max_pod = int(sys.argv[4])
+filename = sys.argv[5]
+# desired_latency = 5000
+# min_pod = 1
+# max_pod = 20
+# filename = os.getcwd()+"/scale.json"
 
-filename = os.getcwd()+"/scale.json"
 #metrics_url = "http://10.32.1.24:8000"
 
 #function to be used when extrating metrics from monitoring pod
@@ -24,7 +28,7 @@ def auto_scale():
     desired_pod = compute_desired_pods(current_latency, desired_latency, current_pod_count, min_pod, max_pod)
     print("Desired pods", desired_pod)
 
-    modify_pod_count(desired_pod)
+    set_pod_count(desired_pod)
 
 def get_current_pod_count():
     with open(filename, 'r') as datafile:
@@ -33,11 +37,12 @@ def get_current_pod_count():
         print("Current pods", current_pod_count)
     return current_pod_count
 
-def modify_pod_count(pod_count):
+def set_pod_count(pod_count):
     with open(filename, 'r') as datafile:
         data = json.load(datafile)
-        data["spec"]["replicas"] = pod_count
-    os.remove(filename)
+        # data["spec"]["replicas"] = pod_count
+    data["spec"]["replicas"] = pod_count
+    # os.remove(filename)
     with open(filename, 'w') as datafile:
         json.dump(data, datafile, indent=4)
 
