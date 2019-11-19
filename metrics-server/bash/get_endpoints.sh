e73b66f88ad0db3bcf95b1bc204bd941ab5663e6
anushiyat <anushiya.16@cse.mrt.ac.lk>
2019-11-19 15:50:41 +0530

    renaming files

--- metrics-server/bash/get_endpoints.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-URL="https://kubernetes/api/v1/namespaces/default/endpoints/ballerina-prime-testing-svc"
-TOKEN=/var/run/secrets/kubernetes.io/serviceaccount/token
-CA_CERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-
-echo "Time Now: `date +%H:%M:%S`"
-curl --cacert ${CA_CERT} -H 'Accept: application/json' -H "Authorization: Bearer $(cat ${TOKEN})" ${URL} > endpoints.json
-    
