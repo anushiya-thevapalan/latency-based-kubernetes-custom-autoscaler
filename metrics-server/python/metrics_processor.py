fc3b6054b7098ba1bfba43519b37596542ad1cc7
anushiyat <anushiya.16@cse.mrt.ac.lk>
2019-11-19 13:49:42 +0530

    commenting the implementation of metrics server and minor improvements

--- metrics-server/python/metrics_processor.py
+++ metrics-server/python/metrics_processor.py
@@ -6,34 +6,32 @@ import os
 import datetime
 from metrics_collector import *
 
+#Files to which metrics should be logged
 latency_stats_file = "/home/metrics-server/python/latency_stats.csv"
 latency_of_all_pods_file = "/home/metrics-server/python/latency_of_all_pods_file.csv"
 
 time_now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
 def get_metrics():
-    get_endpoints()
-    filename = "../bash/endpoints.json"
 
-    ip_address = get_replicas_ip(filename)
+    ip_address = get_replicas_ip()
     latency_of_all_pods = []
 
+    #Collecting metrics from individual pods
     for ip in ip_address:
-        #Collecting metrics from individual pods
         metrics = query_metrics(ip)
-        metrics["timestamp"] = time_now
         print(metrics)
-            
+
+        #Logging metrics of  each pod to a file
         with open(latency_of_all_pods_file, "a+") as csv_file:
             writer = csv.writer(csv_file, delimiter=',')
             values =  [ip, time_now, metrics["99per"], metrics["requests"], metrics["std_dev"]] 
             writer.writerow(values)
                 
-        # metrics_of_all_pods.append(metrics)
-        print(metrics["99per"])
         latency_of_all_pods.append(metrics["99per"])
     average_latency = get_average_latency(latency_of_all_pods)
 
+    #Logging average latency to a file
     with open(latency_stats_file, "a+") as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         values =  [time_now, average_latency, len(ip_address)] 
@@ -41,12 +39,11 @@ def get_metrics():
 
     return average_latency
 
-def get_endpoints():
-    subprocess.call(['../bash/get_endpoints.sh'])
-    return 
-
-def get_replicas_ip(filename):
+def get_replicas_ip():
     ip_address = []
+    subprocess.call(['../bash/get_endpoints.sh'])
+    filename = "../bash/endpoints.json"
+
     with open(filename, 'r') as datafile:
         data = json.load(datafile)
         address = data["subsets"][0]["addresses"]
@@ -54,6 +51,6 @@ def get_replicas_ip(filename):
             ip_address.append(i["ip"])
     return ip_address
 
+#Compute average latency of all pods
 def get_average_latency(latency_of_all_pods):
-    # x = np.percentile(latency_of_all_pods,99)
     return sum(latency_of_all_pods)/len(latency_of_all_pods)
